bf33e9d2b3ff0343633a6e2b960fff46ba3561f8
TormasiPetra <tormasi.petra@gmail.com>
2023-09-15 20:46:18 +0200

    Friday session changes

---
+++ frontend/script.js
@@ -0,0 +1,16 @@
+console.log ("Hello world");
+console.log ("I am Petra");
+//setInterval (()=>console.log("Hello"),1000);
+
+let x  = 10; //ez egy változó
+let y = 5;
+//console.log (10);
+//console.log (x);
+//console.log (x+y);
+//let a = (x + y);
+//console.log (a);
+let z = "csemege";
+console.log (z);
+z = "Bögre"; //megváltoztattuk a z változót 
+console.log (z);
+//const s = ... -- ez egy konstans 
